b79f92d7f1b044a4be5315c0281e4bdd06169fac
liudehua <liudehua@163.com>
2019-02-28 15:24:40 +0800

    Demo

---
+++ user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package com.atguigu.gmall.user.service.impl;
+
+
+
+
+import com.atguigu.gmall.bean.UserAddress;
+import com.atguigu.gmall.bean.UserInfo;
+import com.atguigu.gmall.service.UserService;
+import com.atguigu.gmall.user.mapper.UserAddressMapper;
+import com.atguigu.gmall.user.mapper.UserInfoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserInfoMapper userInfoMapper;
+
+    @Autowired
+    UserAddressMapper userAddressMapper;
+
+    @Override
+    public List<UserInfo> getAllUser() {
+
+        List<UserInfo> userInfos  = userInfoMapper.selectAllUser();
+
+        return userInfos;
+    }
+
+    @Override
+    public List<UserAddress> getAllAddress() {
+
+        List<UserAddress> userAddresses= userAddressMapper.selectAll();
+
+        return userAddresses;
+    }
+
+    @Override
+    public List<UserInfo> allUser() {
+       return userInfoMapper.selectAll();
+    }
+
+
+}
